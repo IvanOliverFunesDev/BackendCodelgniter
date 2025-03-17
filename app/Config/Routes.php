8fca6ad54a3a0d6384a69ab813bcc21a27b12a99
Ivan-Adaits <ioliverfunes@adaits.es>
2025-03-17 19:32:36 +0100

    feat: add CORS filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,6 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('test-cors', function () {
+    return response()->setJSON(['message' => 'CORS funciona correctamente']);
+});
---
+++ app/Filters/CorsFilter.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class CorsFilter implements FilterInterface{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // Configurar los encabezados CORS
+        header("Access-Control-Allow-Origin: *"); // Permitir desde cualquier origen
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+        if ($request->getMethod() === 'options') {
+            exit;
+        }
+    }
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        
+    }
+}
